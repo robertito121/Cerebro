73f4b00c650defe70099a1ebd581324c2eb1f303
robertito121 <37167135+robertito121@users.noreply.github.com>
2020-11-11 00:27:58 -0500

    adding Logger object and logging CognitoUser methods

--- administrative_module/cognito.py
+++ administrative_module/cognito.py
@@ -1,20 +1,23 @@
 from pycognito import Cognito
+from helper_module.helper import Helper
 
 
 class CognitoUser:
-
-    USER_POOL_ID = ""
-    CLIENT_ID = ""
-    CLIENT_SECRET = ""
+    USER_POOL_ID = ''
+    CLIENT_ID = ''
+    CLIENT_SECRET = ''
+    log = Helper.get_logger()
 
     def __init__(self):
-        print("initializing CognitoUser")
+        self.log.info("initializing CognitoUser")
 
     def register_user(self, username, password, email, first_name, middle_name, last_name, phone_number):
         is_registered = False
         cognito = Cognito(self.USER_POOL_ID, self.CLIENT_ID,
                           client_secret=self.CLIENT_SECRET,
                           username=username)
+
+        self.log.info("registering user with username " + username)
         try:
 
             cognito.set_base_attributes(email=email,
@@ -24,12 +27,12 @@ class CognitoUser:
                                         phone_number=phone_number)
             cognito.register(username=username, password=password)
             is_registered = True
+            self.log.info("username " + username + " registered successfully")
         except Exception as e:
             if "UsernameExistException" in str(e):
-                print("Username already exist, please user another username")
+                self.log.error("Username already exist, please user another username")
             else:
-                print(e)
-
+                self.log.error(str(e))
         return is_registered
 
     def confirm_signup(self, username, confirmation_code):
@@ -37,12 +40,13 @@ class CognitoUser:
         cognito = Cognito(self.USER_POOL_ID, self.CLIENT_ID,
                           client_secret=self.CLIENT_SECRET,
                           username=username)
+        self.log.info("confirming user=" + username + " with registration code " + confirmation_code)
         try:
             cognito.confirm_sign_up(confirmation_code, username=username)
             signup_confirmed = True
+            self.log.info("user = " + username + " has been confirmed ")
         except Exception as e:
-            print(e)
-
+            self.log.error(str(e))
         return signup_confirmed
 
     def authenticate_user(self, username, password):
@@ -51,9 +55,11 @@ class CognitoUser:
                           self.CLIENT_ID,
                           client_secret=self.CLIENT_SECRET,
                           username=username)
+        self.log.info("Authenticating user = " + username)
         try:
             cognito.authenticate(password=password)
             is_authenticated = True
+            self.log.info("user = " + username + " has been authenticated")
         except Exception as e:
-            print(e)
+            self.log.error(str(e))
         return is_authenticated
---
+++ helper_module/helper.py
@@ -0,0 +1,15 @@
+import logging
+
+
+class Helper:
+
+    @staticmethod
+    def get_logger():
+        log = logging.getLogger("cerebro_application")
+        handler = logging.FileHandler('/tmp/cerebro.log')
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        handler.setFormatter(formatter)
+        log.addHandler(handler)
+        log.setLevel(logging.DEBUG)
+        return log
+
