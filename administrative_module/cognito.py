d7ba96cab7f4ffe894e568ee41b5c055a2ea7982
pancake757 <74277551+pancake757@users.noreply.github.com>
2020-11-18 20:16:10 -0500

    Update cognito.py

--- administrative_module/cognito.py
+++ administrative_module/cognito.py
@@ -2,6 +2,7 @@ from pycognito import Cognito
 from helper_module.helper import Helper
 
 
+
 class CognitoUser:
     USER_POOL_ID = ''
     CLIENT_ID = ''
@@ -63,3 +64,37 @@ class CognitoUser:
         except Exception as e:
             self.log.error(str(e))
         return is_authenticated
+    ##########################################################################################################################
+    ###GRANTS SECTION###GRANTS SECTION###GRANTS SECTION###GRANTS SECTION###GRANTS SECTION###GRANTS SECTION###GRANTS SECTION###
+    ##########################################################################################################################
+    try: #Yeah this will be moved to the top
+        from PIL import Image #Imports the necessary files to get pytesseract to work#
+    except ImportError:
+        import Image
+    import pytesseract    
+        
+    def ocr(image_file):#Grant's Module
+    #This function will handle the image character recognition and storage
+        input_string = pytesseract.image_to_string(Image.open(imgage_file))#This converts the picture into a string
+        with open('input_string.txt', 'w') as f:#This creates a file to store the input string into the folder that the application is installed
+            f.write("input_string %d" % input_string)
+    def translate():#Grant's storage module
+        LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'#This section is used to brute force the code
+        for key in range(len(LETTERS)):
+           translated = ''
+           for symbol in input_string:
+              if symbol in LETTERS:
+                 num = LETTERS.find(symbol)
+                 num = num - key
+                 if num < 0:
+                    num = num + len(LETTERS)
+                 translated = translated + LETTERS[num]
+              else:
+                 translated = translated + symbol#End of bruteforce
+            if translated(lang="eng+fra+ger+ita"):#Breaks out of loop if there is a language hit
+                final_output=translated
+                break
+            print('Hacking key #%s: %s' % (key, translated))#This is for testing
+        with open('final_output.txt', 'w') as f:#This creates a file to store the input string into the folder that the application is installed
+            f.write("final_output %d" % final_output)
+        
