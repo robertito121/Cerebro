2327fa4d77bab8ca5281aed755626da4b3ea4f91
pancake757 <74277551+pancake757@users.noreply.github.com>
2020-12-01 21:08:16 -0500

    Update OCR.py

--- administrative_module/OCR.py
+++ administrative_module/OCR.py
@@ -1,28 +1,19 @@
+import pytesseract as tesseract
+tesseract.pytesseract.tesseract_cmd= r'C:\Users\grant\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+from PIL import Image
+from langdetect import detect
+#img=Image.open(r"C:\Users\grant\Documents\GitHub\Cerebro\administrative_module\untitled.png")
+#text=tesseract.image_to_string(img)
+#print(text)
+      
+def ocr(image_file_path):#Grant's Module
+#This function will handle the image character recognition and storage
+    input_string = tesseract.image_to_string(Image.open(image_file_path))#This converts the picture into a string
     input_string=input_string[:-1].strip()
     return input_string
 
 #ocr(r"C:\Users\grant\Documents\GitHub\Cerebro\administrative_module\untitled.png")
 
-#def translate(input_file):#Grant's storage module
-#        LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'#This section is used to brute force the code
-#        for key in range(len(LETTERS)):
-#           translated = ''
-#           for symbol in input_string:
-#             if symbol in LETTERS:
-#                 num = LETTERS.find(symbol)
-#                 num = num - key
-#                 if num < 0:
-#                    num = num + len(LETTERS)
-#                 translated = translated + LETTERS[num]
-#             else:
-#                 translated = translated + symbol#End of bruteforce
-#              if translated(lang="eng+fra+ger+ita"):#Breaks out of loop if there is a language hit
-##                 final_output=translated
- ##                break
-#           print('Hacking key #%s: %s' % (key, translated))#This is for testing
-#        with open('final_output.txt', 'w') as f:#This creates a file to store the input string into the folder that the application is installed
-#           f.write("final_output %d" % final_output)
-#translate(ocr(r"C:\Users\grant\Documents\GitHub\Cerebro\administrative_module\untitled.png"))
 def crack(ocr_string):
     #message = 'GUVF VF ZL FRPERG ZRFFNTR.'#This is here for testing purposes
     LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'#This is the alpahabet that will be used for the ceaser cipher
@@ -39,8 +30,9 @@ def crack(ocr_string):
             else:
                 translated = translated + symbol 
         
-        #print('Key #%s: %s' % (key, translated),final_output) #This is for test printing
+        print('Key #%s: %s' % (key, translated)) #This is for test printing
         final_output+=translated+'\n'
+    #print(final_output)
     return final_output
         
         
